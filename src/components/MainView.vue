d4fe94227410ade0378db7548a40df4ef2a26e9a
--global <techlord18@gmail.com>
2021-12-09 12:39:32 -0500

    initial MainView work

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,9 +1,50 @@
 <template>
-    <p>Do the mario</p>
+    <div class="full-container">
+        <div class="playinglist-display">
+            <draggable
+              :list="playingList"
+              group="songs"
+              itemKey="name">
+                <template #item="{ element }">
+                    <div class="song-display"> {{element.name}}</div>
+                </template>
+            </draggable>
+        </div>
+    </div>
 </template>
 
 <script>
+import draggable from "vuedraggable"
 export default {
-    name: "MainView"
+    name: "MainView",
+    components: {
+        draggable
+    },
+    data() {
+        return {
+            playingList: [{name: "Hooked on a Feeling", artist: "Blue Sweede"}],
+            userPlaylists: [],
+            searchList: []
+        };
+    },
+    methods: {
+
+    },
+    created() {
+
+    }
 }
 </script>
+
+<style scoped>
+.full-container {
+    display: flex;
+    flex-direction: row;
+}
+
+.song-container {
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
