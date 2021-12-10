1f11faedefa79cbcd90085f678c573fd8b9a8154
--global <techlord18@gmail.com>
2021-12-10 13:31:10 -0500

    added fetching user spotify playlists

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -10,28 +10,71 @@
                 </template>
             </draggable>
         </div>
+        <div class="inputLists-display">
+            <!--
+            <div class="songSearch-display">
+                <draggable
+                :list="searchList"
+                group="songs"
+                itemKey="name">
+                    <template #item="{ element }">
+                        <div class="song-display"> {{element.name}}</div>
+                    </template>
+                </draggable>
+            </div>
+            -->
+            <div class="userPlaylists-display">
+                <ul>
+                    <button @click="logPlaylists">print playlists</button>
+                    <li v-for="playlist in userPlaylists" :key="playlist.name">
+                        {{playlist.name}}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import draggable from "vuedraggable"
+import SpotifyWebApi from '../spotify-web-api';
 export default {
     name: "MainView",
+    props: {
+        api: SpotifyWebApi
+    },
     components: {
         draggable
     },
     data() {
         return {
-            playingList: [{name: "Hooked on a Feeling", artist: "Blue Sweede"}],
-            userPlaylists: [],
+            userID: "",
+            playingList: [{name: "Hooked on a Feeling", artist: "Blue Sweede"},{name: "Kickstart my Heart", artist: "Motley Crue"}],
+            userPlaylists: [{name: "please do the thing"}],
             searchList: []
         };
     },
     methods: {
-
+        logPlaylists() {
+            console.log(this.userPlaylists[0])
+        }
     },
     created() {
-
+        this.api.getMe().then((value) => {this.userID = value.id; return value.id}).then(
+            (value)=> {return this.api.getUserPlaylists(value)}).then(
+                (value) => {
+                    console.log(value.items)
+                    let outList = []
+                    value.items.forEach(element => {
+                        if(element.owner.display_name != 'Spotify') {
+                            outList.push({name: element.name})
+                        }
+                        console.log(element.name)
+                    })
+                    console.log(outList)
+                    this.userPlaylists = outList
+                })
+        
     }
 }
 </script>
